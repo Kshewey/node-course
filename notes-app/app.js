68b325ae04c13fa7678ce2342e3a115eb1096a0e
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-09-21 17:17:57 -0700

    setting up envirnoment, NPM packages, and general testing (chalk installed)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,11 @@
-const add = require('./utils.js')
+const chalk = require('chalk');
+
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+
+
+console.log(chalk.bold.inverse.green('Success!'));

ba595afd7f90c29d6c128413b1b82005e2dddcb0
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-10-08 23:29:08 -0700

    MapBox API configuration testing

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,12 @@ request({ url: url, json: true }, (error, response) => {
   console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out.  There is a ' + response.body.currently.precipProbability + '% chance of rain')
 
 })
+
+
+const mapBoxURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia3NoZXdleSIsImEiOiJjazFnb29mcmgwMjVsM2l0ZjhnOGJzMDN5In0.edL7638VFPHORKBsovXREw"
+
+request({ url: mapBoxURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1]
+  const longitude = response.body.features[0].center[0]
+  console.log(latitude, longitude)
+})

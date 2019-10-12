60e970da7db6fa1dfd6ed5d5baefc2ad93ebff39
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-10-11 20:59:04 -0700

    added functionality to get location from user

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,19 +2,29 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
+console.log(process.argv[2])
+if (!address) {
+  console.log('Please provide a location!')
+} else {
+
+geocode(address, (error, data) => {
+  if (error) {
+    return console.log(error)
+  }
+
+
+  forecast(data.longitude, data.latitude, (error, forecastData) => {
+
+  if (error) {
+    return console.log(error)
+  }
+
+  console.log(data.location)
+  console.log(forecastData)
+  })
 
 
 
-geocode("Los Angeles", (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
 })
-
-
-
-
-
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+}

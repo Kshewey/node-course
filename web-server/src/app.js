8df1ba66690ee98a53fe28698fc1b8539f0b0b23
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-10-16 22:07:07 -0700

    Setup the HTTP Endpoint for weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 
 
 
@@ -50,11 +53,36 @@ if(!req.query.address) {
     error: 'You must provide a location'
   })
 }
-res.send({
-    forecast: "It's hot for October",
-    location: "Right here",
-    address: req.query.address
+
+geocode(req.query.address, (error, { latitude, longitude, location} ) => {
+  if (error) {
+    return res.send({ error })
+  }
+
+  forecast(latitude, longitude, (error, forecastData) => {
+    if (error) {
+      return res.send({ error })
+    }
+    res.send({
+      forecast: forecastData,
+      location,
+      address: req.query.address
+    })
   })
+})
+
+
+
+
+
+
+          
+
+// res.send({
+//     forecast: "It's hot for October",
+//     location: "Right here",
+//     address: req.query.address
+//   })
 })
 
 
